d0d0a1ae7dbba8860cee4e3cc2ff79f6a3c86607
Hamo <hassonhamo3@gmail.com>
2026-03-16 15:33:53 +0300

    Disiplaying credit score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : " ";
+
+  return (
+    <Badge color={color} fontSize={16} padding={2}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
